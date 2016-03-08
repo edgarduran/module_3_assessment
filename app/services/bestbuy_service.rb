456c05fbb5cbcff2ea675f716b5c46d3575ae672
Edgar Duran <edgarduran@Edgars-MacBook-Pro-8.local>
2016-03-08 11:36:06 -0700

    add method for advanced search

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -12,7 +12,20 @@ class BestbuyService
 
 
   def product_search(params)
-    parse_json(connection.get("products(search=#{params[:search]})?show=sku,name,salePrice,shortDescription,image,customerReviewAverage&format=json&pageSize=15&page=1&apiKey=ubhx8perq9r8xj9hukpe66xr"))
+    if params[:search].split(' ').size == 1
+      parse_json(connection.get("products(search=#{params[:search]})?show=sku,name,salePrice,shortDescription,image,customerReviewAverage&format=json&pageSize=15&page=1&apiKey=ubhx8perq9r8xj9hukpe66xr"))
+    else
+      multi_word_search(params)
+    end
+  end
+
+  def multi_word_search(params)
+    words = params[:search].split(' ').map do |word|
+      "search=" + word + "&"
+    end.join
+    search = words.slice(0..-2)
+    binding.pry
+    parse_json(connection.get("products(search=#{search})?show=sku,name,salePrice,shortDescription,image,customerReviewAverage&format=json&pageSize=15&page=1&apiKey=ubhx8perq9r8xj9hukpe66xr"))
   end
 
   private
