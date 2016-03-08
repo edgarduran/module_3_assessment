a21cea530a3b5a67c20d70ebe2284ef991872ca8
Edgar Duran <edgarduran@Edgars-MacBook-Pro-8.local>
2016-03-08 09:15:57 -0700

    add api routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: [:index, :show], defaults: { format: 'json' }
+    end
+  end
+
   root 'items#index'
 
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
+
 end
---
+++ test/controllers/api/v1/items_controller_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class Api::V1::ItemsControllerTest < ActionController::TestCase
+  def test_index
+    get :index
+    assert_response :success
+  end
+
+  def test_show
+    get :show
+    assert_response :success
+  end
+
+end
